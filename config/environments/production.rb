8b23a3d00d9e13368e97e25ea921b3f41316903a
chaychee <chaychee@users.noreply.github.com>
2014-11-12 00:56:44 +0000

    Configured app for Production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Bloccit::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
@@ -79,8 +79,5 @@ Bloccit::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.action_mailer.default_url_options = { host: 'chaychee-bloccit.herokuapp.com'}
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-
 
 end
