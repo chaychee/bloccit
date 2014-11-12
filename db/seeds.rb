8a70d0d8cbcc0f2e80011833ab453354ed5ce36e
chaychee <chaychee@users.noreply.github.com>
2014-11-12 00:41:04 +0000

    Associated and nested topics and posts.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,13 @@
 require 'faker'
  
 # Create users
+# Note: by calling `User.new` instead of `create`,
+# we create an instance of User which isn't immediately saved to the database.
+
+# The `skip_confirmation!` method sets the `confirmed_at` attribute
+# to avoid triggering an confirmation email when the User is saved.
+
+# The `save` method then saves this User to the database.
 5.times do
   user = User.new(
     name:   Faker::Name.name,
@@ -12,13 +19,14 @@ require 'faker'
 end
 users=User.all
 
-# Note: by calling `User.new` instead of `create`,
-# we create an instance of User which isn't immediately saved to the database.
-
-# The `skip_confirmation!` method sets the `confirmed_at` attribute
-# to avoid triggering an confirmation email when the User is saved.
-
-# The `save` method then saves this User to the database.
+# Create Topics
+5.times do
+  Topic.create!(
+      name:   Faker::Lorem.sentence,
+      description:  Faker::Lorem.paragraph
+    )
+end
+topics=Topic.all
 
 
 # Create Posts
@@ -26,7 +34,8 @@ users=User.all
   Post.create!(
     user:   users.sample,
     title:  Faker::Lorem.sentence,
-    body:   Faker::Lorem.paragraph
+    body:   Faker::Lorem.paragraph,
+    topic:  topics.sample
  )
 end
 posts = Post.all
@@ -43,23 +52,32 @@ end
 user = User.find(1)
 user.skip_reconfirmation!
 user.update_attributes!(
-  name: 'JG',
-  email: 'jg@jg.com',
-  password: 'asdfasdf'
-)
-
-user = User.find(2)
-user.skip_reconfirmation!
-user.update_attributes!(
-  name: 'Admin',
+  name: 'Admin User',
   email: 'admin@jg.com',
   password: 'asdfasdf',
   role: 'admin'
 )
 
+user = User.find(2)
+user.skip_reconfirmation!
+user.update_attributes!(
+  name: 'Moderator User',
+  email: 'moderator@jg.com',
+  password: 'asdfasdf',
+  role: 'moderator'
+)
+
+user = User.find(3)
+user.skip_reconfirmation!
+user.update_attributes!(
+  name: 'Member User',
+  email: 'member@jg.com',
+  password: 'asdfasdf'
+)
  
 puts "Seed finished"
 puts "#{User.count} users created"
+puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
  
