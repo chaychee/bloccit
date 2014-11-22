d113f9c216c71ffdaf64cbc29458c525232ed49a
chaychee <chaychee@users.noreply.github.com>
2014-11-21 22:04:00 -0800

    initial checkin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,17 @@ module ApplicationHelper
     (redcarpet.render text).html_safe
   end
 
+
+  def up_vote_link_classes(post)
+    "glyphicon glyphicon-chevron-up#{
+      current_user.voted(post) && current_user.voted(post).up_vote? ? " voted" : nil
+      }"
+  end
+
+  def down_vote_link_classes(post)
+    "glyphicon glyphicon-chevron-down#{
+      current_user.voted(post) && current_user.voted(post).down_vote? ? " voted" : nil
+      }"
+  end
+
 end
